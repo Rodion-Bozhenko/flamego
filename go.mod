072aa36d1628c5512a9fa54badd45afeee5d75bf
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-10-17 15:54:16 +0200

    Create firestore collections preview with tables

--- go.mod
+++ go.mod
@@ -2,19 +2,53 @@ module github.com/Rodion-Bozhenko/flame-go
 
 go 1.21.1
 
-require github.com/AllenDang/giu v0.7.0
+require (
+	cloud.google.com/go/firestore v1.13.0
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/AllenDang/giu v0.7.0
+	github.com/sqweek/dialog v0.0.0-20220809060634-e981b270ebbf
+	google.golang.org/api v0.143.0
+)
 
 require (
+	cloud.google.com/go v0.110.7 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.1 // indirect
+	cloud.google.com/go/longrunning v0.5.1 // indirect
+	cloud.google.com/go/storage v1.33.0 // indirect
 	github.com/AllenDang/go-findfont v0.0.0-20200702051237-9f180485aeb8 // indirect
 	github.com/AllenDang/imgui-go v1.12.1-0.20221124025851-59b862ca5a0c // indirect
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
 	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.1 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/mazznoer/csscolorparser v0.1.3 // indirect
 	github.com/napsy/go-css v0.0.0-20221107082635-4ed403047a64 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/image v0.12.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/eapache/queue.v1 v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,60 +1,166 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"sort"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
 	g "github.com/AllenDang/giu"
+	"github.com/sqweek/dialog"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 )
 
 var (
-	leftMenuWidth   float32 = 200
-	selectedProject string
+	leftMenuWidth      float32 = 200
+	selectedCollection string
+	app                *firebase.App
+	firebaseCtx        = context.Background()
+	collectionsRef     []*firestore.CollectionRef
+	client             *firestore.Client
+	docsIter           *firestore.DocumentIterator
+	columns            []*g.TableColumnWidget
+	rows               []*g.TableRowWidget
+	docs               []map[string]interface{}
 )
+
+func main() {
+	w := g.NewMasterWindow("Flamego", 1000, 700, g.MasterWindowFlagsFloating)
+	w.Run(loop)
+}
 
 func loop() {
 	g.SingleWindow().Layout(
 		g.SplitLayout(g.DirectionVertical, &leftMenuWidth,
 			g.Layout{
-				g.Label("Projects"),
-				NewProjectButton("Project One"),
-				NewProjectButton("Project Two"),
-				NewProjectButton("Poject Three"),
+				g.Label("Collections"),
+				g.Column(RenderCollectionButtons(collectionsRef)...),
 			},
 			g.Layout{
 				g.Label("Main Frame"),
-				renderItemsForProject(),
+				g.Button("Add service account file").OnClick(func() {
+					path, err := PromptServiceAccountPath()
+					if err != nil {
+						g.Msgbox("Something went wrong", fmt.Sprintf("Cannot load file: %s", err.Error()))
+						return
+					}
+					app, err = LogApp(path)
+					if err != nil {
+						g.Msgbox("Error", fmt.Sprintf("Cannot initialize app: %s\n", err.Error()))
+					}
+
+					client, err = app.Firestore(firebaseCtx)
+					if err != nil {
+						g.Msgbox("Firestore Error", fmt.Sprintf("Cannot connect to Firestore: %s\n", err.Error()))
+					}
+
+					collectionsRef, err = client.Collections(firebaseCtx).GetAll()
+					if err != nil {
+						g.Msgbox("Firestore Error", fmt.Sprintf("Cannot retrieve all collections: %s\n", err.Error()))
+					}
+				}),
+				RenderDocsTable(docsIter),
+				g.PrepareMsgbox(),
 			},
 		),
 	)
 }
 
-func NewProjectButton(title string) *g.ButtonWidget {
+func RenderCollectionButtons(colls []*firestore.CollectionRef) []g.Widget {
+	buttons := make([]g.Widget, 0, len(colls))
+	for _, coll := range colls {
+		buttons = append(buttons, NewCollectionButton(coll.ID))
+	}
+	return buttons
+}
+
+func RenderDocsTable(docsIter *firestore.DocumentIterator) g.Widget {
+	if docsIter == nil {
+		return g.Label("")
+	}
+
+	if len(docs) == 0 {
+		for {
+			docSnap, err := docsIter.Next()
+			if errors.Is(err, iterator.Done) {
+				docsIter.Stop()
+				break
+			}
+			if err != nil {
+				docsIter.Stop()
+				panic(err.Error())
+			}
+			data := docSnap.Data()
+			if data != nil {
+				docs = append(docs, data)
+			}
+		}
+	}
+
+	var keys []string
+
+	for key := range docs[0] {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	if len(columns) == 0 {
+		for _, key := range keys {
+			columns = append(columns, g.TableColumn(key))
+		}
+	}
+
+	if len(rows) == 0 {
+		for _, doc := range docs {
+			var rowCells []g.Widget
+			for _, k := range keys {
+				rowCells = append(rowCells, g.Label(fmt.Sprintf("%v", doc[k])))
+			}
+			rows = append(rows, g.TableRow(rowCells...))
+		}
+	}
+
+	return g.Table().FastMode(true).Columns(columns...).Rows(rows...)
+}
+
+func getAllDocs(client *firestore.Client, collId string) *firestore.DocumentIterator {
+	if client != nil {
+		return client.Collection(collId).Documents(firebaseCtx)
+	}
+	return nil
+}
+
+func NewCollectionButton(title string) *g.ButtonWidget {
 	return g.Button(title).OnClick(func() {
-		selectedProject = title
+		selectedCollection = title
+		docsIter = getAllDocs(client, title)
+		docs = []map[string]interface{}{}
+		columns = []*g.TableColumnWidget{}
+		rows = []*g.TableRowWidget{}
 	})
 }
 
-func renderItemsForProject() g.Widget {
-	switch selectedProject {
-	case "Project One":
-		return g.Layout{
-			g.Label("Item 1 for Project One"),
-			g.Label("Item 2 for Project One"),
+func PromptServiceAccountPath() (string, error) {
+	path, err := dialog.File().Filter("Service account json file", "json").Load()
+	if err != nil {
+		if err == dialog.ErrCancelled {
+			return "", nil
 		}
-	case "Project Two":
-		return g.Layout{
-			g.Label("Item 1 for Project Two"),
-			g.Label("Item 2 for Project Two"),
-		}
-	case "Poject Three":
-		return g.Layout{
-			g.Label("Item 1 for Project Three"),
-			g.Label("Item 2 for Project Three"),
-		}
-	default:
-		return g.Label("Select a project to view items.")
+		return "", err
 	}
+	return path, nil
 }
 
-func main() {
-	w := g.NewMasterWindow("Flamego", 1000, 700, g.MasterWindowFlagsFloating)
-	w.Run(loop)
+func LogApp(path string) (*firebase.App, error) {
+	opt := option.WithCredentialsFile(path)
+	app, err := firebase.NewApp(firebaseCtx, nil, opt)
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
 }
