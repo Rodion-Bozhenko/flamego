124e45f9834b84acd2ca37ea2dd0e2b735f1dacf
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-12-30 16:23:11 +0100

    Fix linter warnings

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Flamego - GUI for firestore
 package main
 
 import (
@@ -14,6 +15,8 @@ import (
 	"google.golang.org/api/option"
 )
 
+type doc = map[string]interface{}
+
 var (
 	leftMenuWidth      float32 = 200
 	selectedCollection string
@@ -24,30 +27,33 @@ var (
 	docsIter           *firestore.DocumentIterator
 	columns            []*g.TableColumnWidget
 	rows               []*g.TableRowWidget
-	docs               []map[string]interface{}
+	docs               []doc
+	editingDoc         doc
 )
 
 func main() {
-	w := g.NewMasterWindow("Flamego", 1000, 700, g.MasterWindowFlagsFloating)
+	w := g.NewMasterWindow("Flamego", 1000, 700, 0)
 	w.Run(loop)
 }
 
 func loop() {
 	g.SingleWindow().Layout(
+		g.Popup("editing-doc-modal").Layout(g.Label("This is a popup"), g.Button("Close").OnClick(func() { g.CloseCurrentPopup() })),
+		g.Buttonf("Click").OnClick(func() { g.OpenPopup("editing-doc-modal") }),
 		g.SplitLayout(g.DirectionVertical, &leftMenuWidth,
 			g.Layout{
 				g.Label("Collections"),
-				g.Column(RenderCollectionButtons(collectionsRef)...),
+				g.Column(renderCollectionButtons(collectionsRef)...),
 			},
 			g.Layout{
 				g.Label("Main Frame"),
 				g.Button("Add service account file").OnClick(func() {
-					path, err := PromptServiceAccountPath()
+					path, err := promptServiceAccountPath()
 					if err != nil {
 						g.Msgbox("Something went wrong", fmt.Sprintf("Cannot load file: %s", err.Error()))
 						return
 					}
-					app, err = LogApp(path)
+					app, err = logApp(path)
 					if err != nil {
 						g.Msgbox("Error", fmt.Sprintf("Cannot initialize app: %s\n", err.Error()))
 					}
@@ -62,22 +68,23 @@ func loop() {
 						g.Msgbox("Firestore Error", fmt.Sprintf("Cannot retrieve all collections: %s\n", err.Error()))
 					}
 				}),
-				RenderDocsTable(docsIter),
+				renderDocsTable(docsIter),
 				g.PrepareMsgbox(),
 			},
 		),
 	)
 }
 
-func RenderCollectionButtons(colls []*firestore.CollectionRef) []g.Widget {
+func renderCollectionButtons(colls []*firestore.CollectionRef) []g.Widget {
 	buttons := make([]g.Widget, 0, len(colls))
 	for _, coll := range colls {
-		buttons = append(buttons, NewCollectionButton(coll.ID))
+		buttons = append(buttons, newCollectionButton(coll.ID))
 	}
+
 	return buttons
 }
 
-func RenderDocsTable(docsIter *firestore.DocumentIterator) g.Widget {
+func renderDocsTable(docsIter *firestore.DocumentIterator) g.Widget {
 	if docsIter == nil {
 		return g.Label("")
 	}
@@ -118,7 +125,10 @@ func RenderDocsTable(docsIter *firestore.DocumentIterator) g.Widget {
 		for _, doc := range docs {
 			var rowCells []g.Widget
 			for _, k := range keys {
-				rowCells = append(rowCells, g.Label(fmt.Sprintf("%v", doc[k])))
+				rowCells = append(rowCells, g.Buttonf("%v", doc[k]).OnClick(func() {
+					editingDoc = doc
+					g.OpenPopup("editing-doc-modal")
+				}))
 			}
 			rows = append(rows, g.TableRow(rowCells...))
 		}
@@ -127,14 +137,18 @@ func RenderDocsTable(docsIter *firestore.DocumentIterator) g.Widget {
 	return g.Table().FastMode(true).Columns(columns...).Rows(rows...)
 }
 
-func getAllDocs(client *firestore.Client, collId string) *firestore.DocumentIterator {
+func renderDocPopup(doc doc) g.Widget {
+	return g.Popup("editing-doc-modal").Layout(g.Label("This is a popup"), g.Button("Close").OnClick(func() { g.CloseCurrentPopup() }))
+}
+
+func getAllDocs(client *firestore.Client, collID string) *firestore.DocumentIterator {
 	if client != nil {
-		return client.Collection(collId).Documents(firebaseCtx)
+		return client.Collection(collID).Documents(firebaseCtx)
 	}
 	return nil
 }
 
-func NewCollectionButton(title string) *g.ButtonWidget {
+func newCollectionButton(title string) *g.ButtonWidget {
 	return g.Button(title).OnClick(func() {
 		selectedCollection = title
 		docsIter = getAllDocs(client, title)
@@ -144,7 +158,7 @@ func NewCollectionButton(title string) *g.ButtonWidget {
 	})
 }
 
-func PromptServiceAccountPath() (string, error) {
+func promptServiceAccountPath() (string, error) {
 	path, err := dialog.File().Filter("Service account json file", "json").Load()
 	if err != nil {
 		if err == dialog.ErrCancelled {
@@ -155,7 +169,7 @@ func PromptServiceAccountPath() (string, error) {
 	return path, nil
 }
 
-func LogApp(path string) (*firebase.App, error) {
+func logApp(path string) (*firebase.App, error) {
 	opt := option.WithCredentialsFile(path)
 	app, err := firebase.NewApp(firebaseCtx, nil, opt)
 	if err != nil {
